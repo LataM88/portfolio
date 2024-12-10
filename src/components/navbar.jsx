f168e6a76e74bcc9875fe2e6cb251fe077417ad4
Michał Łata <latamichal88@gmail.com>
2024-12-10 15:53:35 +0100

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navbar.css';
+import logom from '../logom.svg'; 
+
+const Navbar = () => { 
+    return (
+        <div className="navbar">
+           <img src={logom} alt="" style={{width: '65px', height: '65px'}} />
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About me</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Media</li>
+            </ul>
+            <div className="nav-connect">Connect with me</div>
+        </div>
+    );
+};
+
+export default Navbar;
