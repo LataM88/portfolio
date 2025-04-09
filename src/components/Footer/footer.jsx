534769b4d0ea30469a5ed6db3be1a50a4f403452
LataM88 <latamichal88@gmail.com>
2025-04-09 19:32:54 +0200

    autmatyczna zmiana daty

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             </div>
             <hr />
             <div className="footer-bottom">
-                <p className="footer-bottom-left">© 2024 Michał Łata</p>
+                <p className="footer-bottom-left">© {new Date().getFullYear()} Michał Łata</p>
                 <div className="footer-bottom-right">
                     <p>Term of Services</p>
                     <p>Privacy Policy</p>
